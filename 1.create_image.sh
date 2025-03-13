d3a378e421d75a8ac8eb32e0a553d24b2834d4b7
brian bowen <brian.bowen@mantech.com>
2025-03-13 12:54:13 -0400

    added additional script for npm and nve setup with right version did test npm build-was fine ran image creation and container creation - was fine (problem with fapolicyd still)

--- 1.create_image.sh
+++ 1.create_image.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
 echo you need to be logged into registry.redhat.io
 echo run podman login registry.redhat.io  
 
 #build website
 cd sitesource
+nvm install 18.20.4
+#should have nodejs version 18.20.4 now
+npm install 
 npm run build
 cd ..
 
-#build container
+#build image for containers to use
 podman build -f Dockerfile-freshbuild -t ogs-dashboard:latest
 
 #display container
--- sitesource/buildwebsite.sh
+++ sitesource/buildwebsite.sh
@@ -1,1 +1,3 @@
+npm set strict-ssl false
+nvm install 18.20.4
 npm run build
